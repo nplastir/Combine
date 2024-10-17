042087766881b12766e480a5673c415d27903f58
Nikolaos Plastiras <nipla31@gmail.com>
2024-10-17 08:34:42 +0200

    Updated framework

--- makeImpacts_st4.sh
+++ makeImpacts_st4.sh
@@ -17,26 +17,26 @@ for year in "${years[@]}"; do
 
         if [[ "$channel" == "FH" ]]; then
 
-            plotImpacts.py -i higgsCombine_nominal_obs_impacts.Impacts.mH125p38.json -o higgsCombine_nominal_obs_impacts.Impacts.mH125p38_SF_norm_tt_cc --POI SF_norm_tt_cc_FH_${year}
+            plotImpacts.py -i higgsCombine_nominal_obs_impacts.Impacts.mH125p38.json -o higgsCombine_nominal_obs_impacts.Impacts.mH125p38_SF_norm_tt_cc_FH_${year} --POI SF_norm_tt_cc_FH_${year}
 
-            plotImpacts.py -i higgsCombine_nominal_obs_impacts.Impacts.mH125p38.json -o higgsCombine_nominal_obs_impacts.Impacts.mH125p38_SF_norm_tt_cj --POI SF_norm_tt_cj_FH_${year}
+            plotImpacts.py -i higgsCombine_nominal_obs_impacts.Impacts.mH125p38.json -o higgsCombine_nominal_obs_impacts.Impacts.mH125p38_SF_norm_tt_cj_FH_${year} --POI SF_norm_tt_cj_FH_${year}
 
-            plotImpacts.py -i higgsCombine_nominal_obs_impacts.Impacts.mH125p38.json -o higgsCombine_nominal_obs_impacts.Impacts.mH125p38_SF_norm_tt_bb --POI SF_norm_tt_bb_FH_${year}
+            plotImpacts.py -i higgsCombine_nominal_obs_impacts.Impacts.mH125p38.json -o higgsCombine_nominal_obs_impacts.Impacts.mH125p38_SF_norm_tt_bb_FH_${year} --POI SF_norm_tt_bb_FH_${year}
 
-            plotImpacts.py -i higgsCombine_nominal_obs_impacts.Impacts.mH125p38.json -o higgsCombine_nominal_obs_impacts.Impacts.mH125p38_SF_norm_tt_bj --POI SF_norm_tt_bj_FH_${year}
+            plotImpacts.py -i higgsCombine_nominal_obs_impacts.Impacts.mH125p38.json -o higgsCombine_nominal_obs_impacts.Impacts.mH125p38_SF_norm_tt_bj_FH_${year} --POI SF_norm_tt_bj_FH_${year}
 
-            plotImpacts.py -i higgsCombine_nominal_obs_impacts.Impacts.mH125p38.json -o higgsCombine_nominal_obs_impacts.Impacts.mH125p38_SF_norm_tt_lf --POI SF_norm_tt_lf_FH_${year}
+            plotImpacts.py -i higgsCombine_nominal_obs_impacts.Impacts.mH125p38.json -o higgsCombine_nominal_obs_impacts.Impacts.mH125p38_SF_norm_tt_lf_FH_${year} --POI SF_norm_tt_lf_FH_${year}
 
         else
-            plotImpacts.py -i higgsCombine_nominal_obs_impacts.Impacts.mH125p38.json -o higgsCombine_nominal_obs_impacts.Impacts.mH125p38_SF_norm_tt_cc --POI SF_norm_tt_cc_${year}
+            plotImpacts.py -i higgsCombine_nominal_obs_impacts.Impacts.mH125p38.json -o higgsCombine_nominal_obs_impacts.Impacts.mH125p38_SF_norm_tt_cc_${year} --POI SF_norm_tt_cc_${year}
 
-            plotImpacts.py -i higgsCombine_nominal_obs_impacts.Impacts.mH125p38.json -o higgsCombine_nominal_obs_impacts.Impacts.mH125p38_SF_norm_tt_cj --POI SF_norm_tt_cj_${year}
+            plotImpacts.py -i higgsCombine_nominal_obs_impacts.Impacts.mH125p38.json -o higgsCombine_nominal_obs_impacts.Impacts.mH125p38_SF_norm_tt_cj_${year} --POI SF_norm_tt_cj_${year}
 
-            plotImpacts.py -i higgsCombine_nominal_obs_impacts.Impacts.mH125p38.json -o higgsCombine_nominal_obs_impacts.Impacts.mH125p38_SF_norm_tt_bb --POI SF_norm_tt_bb_${year}
+            plotImpacts.py -i higgsCombine_nominal_obs_impacts.Impacts.mH125p38.json -o higgsCombine_nominal_obs_impacts.Impacts.mH125p38_SF_norm_tt_bb_${year} --POI SF_norm_tt_bb_${year}
 
-            plotImpacts.py -i higgsCombine_nominal_obs_impacts.Impacts.mH125p38.json -o higgsCombine_nominal_obs_impacts.Impacts.mH125p38_SF_norm_tt_bj --POI SF_norm_tt_bj_${year}
+            plotImpacts.py -i higgsCombine_nominal_obs_impacts.Impacts.mH125p38.json -o higgsCombine_nominal_obs_impacts.Impacts.mH125p38_SF_norm_tt_bj_${year} --POI SF_norm_tt_bj_${year}
 
-            plotImpacts.py -i higgsCombine_nominal_obs_impacts.Impacts.mH125p38.json -o higgsCombine_nominal_obs_impacts.Impacts.mH125p38_SF_norm_tt_lf --POI SF_norm_tt_lf_${year}
+            plotImpacts.py -i higgsCombine_nominal_obs_impacts.Impacts.mH125p38.json -o higgsCombine_nominal_obs_impacts.Impacts.mH125p38_SF_norm_tt_lf_${year} --POI SF_norm_tt_lf_${year}
 
         fi
 
